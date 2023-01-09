d16bd427a80922d8b979ee057088824839773836
Bathily Yahaya <developpeur.yb@gmail.com>
2023-01-09 12:28:40 +0100

    maquette

--- fight_game/src/main/java/yahaya_rachelle/App.java
+++ fight_game/src/main/java/yahaya_rachelle/App.java
@@ -1,11 +1,13 @@
 package yahaya_rachelle;
 
+import javafx.application.Application;
+
 class App
 {
     /**
      * @param args
      */
     public static void main(String[] args) {
-
+        
     }
 }
